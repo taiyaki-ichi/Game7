fe4ab7f7b7db948177ed3906dbc3f6b0aff31560
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2020-07-01 20:26:05 +0900

    Playerの不要なコメントアウトを削除

--- SharedProject/Game/Stage/Character/Player.hpp
+++ SharedProject/Game/Stage/Character/Player.hpp
@@ -5,44 +5,6 @@
 #include"Game/Stage/PhysicsModel.hpp"
 
 
-namespace Game::Stage
-{
-	/*
-	class Player : public GravityActor
-	{
-		constexpr static float MAX_HORIZON_SPEED = 5.5f;
-		constexpr static float MAX_VERTICAL_SPEED = 21.f;
-		constexpr static float JUMP_POWER_MAX = 9.f;
-		constexpr static float JUMP_POWER_MIN = 7.f;
-		constexpr static float RUN_POWER = 0.2f;
-		constexpr static float JUMPING_GRAVITY_RATE = 0.8f;
-
-		GameLib::DrawAnimation mAnimation;
-		GameLib::Collider mCollider;
-
-		PhysicsModel mPhysicsModel;
-
-		constexpr static char ON_GROUND_FLAG = 0b00000001;
-		char mFlags;
-
-		int mJumpFlag;
-
-	public:
-		Player(Actor* owner, int updateOrder = 0);
-		virtual ~Player() = default;
-		void CustomizeUpdate() override;
-
-	private:
-		GameLib::Vector2 GetPowerPerFrame();
-		void UpdateAnimation(const GameLib::Vector2& power);
-		void UpdateCollider();
-
-	};
-
-	*/
-}
-
-
 namespace Game::Stage::Player
 {
 	class Actor : public GameLib::Actor
