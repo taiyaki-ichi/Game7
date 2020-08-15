42cf938dde79f055d7eeb5766c4ebc2804c1969a
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2020-08-15 15:37:44 +0900

    Stage001仮

--- StageEditor/StartActor.cpp
+++ StageEditor/StartActor.cpp
@@ -2,6 +2,7 @@
 #include"StageEditor.hpp"
 #include"Stage/Stage.hpp"
 #include"GameLib/include/InputState/InputState.hpp"
+#include"GameLib/include/CollisionDetection/Collider.hpp"
 
 namespace StageEditor
 {
@@ -17,12 +18,16 @@ namespace StageEditor
 	{
 		if (mStageEditor&&mStageEditor->GetCheckFlag())
 		{
+			GameLib::Collider::SetIsDebug(false);
+
 			mStageEditor->SetState(GameLib::Actor::State::Dead);
 			mStageEditor = nullptr;
 			mStage = new Stage::Stage{ this,"tmp.json" };
 		}
 		else if (mStage && GameLib::InputState::GetState(GameLib::Key::no0) == GameLib::ButtonState::Pressed)
 		{
+			GameLib::Collider::SetIsDebug(true);
+
 			mStage->SetState(GameLib::Actor::State::Dead);
 			mStage = nullptr;
 			mStageEditor = new StageEditor{ this };
