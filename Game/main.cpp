0408005a75363702afcff8349620100106a359d5
taiyaki <60641436+taiyaki-ichi@users.noreply.github.com>
2020-09-03 16:14:13 +0900

    windowsアプリケーションに変更

--- Game/main.cpp
+++ Game/main.cpp
@@ -4,21 +4,39 @@
 #include<random>
 #include"GameLib/include/App.hpp"
 #include"Game.hpp"
+#include"Stage/WindowSize.hpp"
+
+#include<Windows.h>
 
 #include <stdlib.h>
 #include <crtdbg.h>
 #define malloc(X) _malloc_dbg(X,_NORMAL_BLOCK,__FILE__,__LINE__) 
 #define new ::new(_NORMAL_BLOCK, __FILE__, __LINE__)
 
+//デバック用
 int main() 
 {
 
 	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_DELAY_FREE_MEM_DF | _CRTDBG_CHECK_ALWAYS_DF | _CRTDBG_LEAK_CHECK_DF);
 
-	auto app = GameLib::CreatAppPtr({ "game",800,600 });
+	auto app = GameLib::CreatAppPtr({ "game",Stage::WindowSize::WIDTH,Stage::WindowSize::HEIGHT });
 	app->Start<Game::Game>();
 
 
 	return 0;
 
+}
+
+
+int CALLBACK WinMain(
+	_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPSTR     lpCmdLine,
+	_In_ int       nCmdShow
+)
+{
+	auto app = GameLib::CreatAppPtr({ "MEME",Stage::WindowSize::WIDTH,Stage::WindowSize::HEIGHT });
+	app->Start<Game::Game>();
+
+	return 0;
 }
